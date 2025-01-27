d938e3e771b88e454ba17a7618feb9da9442873d
Andy Cai <andycaai00@gmail.com>
2025-01-27 12:41:18 -0800

    calc

---
+++ calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to the Calculator program.");
+    printf("Operations - 1:add 2:subtract 3:multiply 4:divide 5:and 6:or 7:xor 8:lshift 9:rshift\n");
+
+
+
+    printf("Numbers of operations performed: %d");
+
+
+
+
+
+
+    printf("Continue (y/n)? : /n");
+    scanf("%d", );
+
+    if 
+
+}
