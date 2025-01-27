4522ebbe5595661b99d7c168fc7078336908fc44
Aram Sodhi <asodhi@calpoly.edu>
2025-01-27 12:39:31 -0800

    Operations.c and header file

---
+++ operations.c
@@ -0,0 +1,47 @@
+/*
+int addnums(int, int);
+int subnums(int, int);
+int multnums(int, int);
+int divnums(int, int);
+int andnums(int, int);
+int ornums(int, int);
+int xornums(int, int);
+int lshiftnums(int, int);
+unsigned int rshiftnums(unsigned int, int);
+*/
+
+int addnums(int a, int b) {
+    return a + b;
+}
+
+int subnums(int a, int b) {
+    return a - b;
+}
+
+int multnums(int a, int b) {
+    return a * b;
+}
+
+int divnums(int a, int b) {
+    return a / b;
+}
+
+int andnums(int a, int b) {
+    return a & b;
+}
+
+int ornums(int a, int b) {
+    return a | b;
+}
+
+int xornums(int a, int b) {
+    return a ^ b;
+}
+
+int lshiftnums(int a, int b) {
+    return a << b;
+}
+
+unsigned int rshiftnums(unsigned int a, int b) {
+    return a >> b;
+}
---
+++ operations.h
@@ -0,0 +1,9 @@
+int addnums(int a, int b);
+int subnums(int a, int b);
+int multnums(int a, int b);
+int divnums(int a, int b);
+int andnums(int a, int b);
+int ornums(int a, int b);
+int xornums(int a, int b);
+int lshiftnums(int a, int b);
+unsigned int rshiftnums(unsigned int a, int b);
